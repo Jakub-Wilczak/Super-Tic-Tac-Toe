376f774030dfdcffd50f86c9bdf9f18c96d5baf3
s21344 <jakub.alek.wilczak@gmail.com>
2023-09-28 01:44:58 +0200

    small prefab changes

--- Assets/Scripts/BigBoard.cs
+++ Assets/Scripts/BigBoard.cs
@@ -1,16 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
-
 public class BigBoard : MonoBehaviour
 {
-    
-    
+    void Awake(){
+        //SetPlayableBackgrounds();
+        
+    }
+
+
     void Update()
     {
-        //transform.Rotate(Time.deltaTime * 120 * new Vector3(0, 1, 0));
     }
     
+
+
+
+    public void SetPlayableBackgrounds()
+    {
+        
+        // List<Transform> list0 = GetChildren(transform.parent);
+        //
+        // foreach (var child in list0)
+        // {
+        //     GetChildren(child).ElementAt(11).gameObject.SetActive(false);
+        // }
+        //
+        // GetChildren(transform).ElementAt(11).gameObject.SetActive(true);
+
+    }
     
     
 }
